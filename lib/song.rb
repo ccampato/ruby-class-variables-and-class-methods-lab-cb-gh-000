2bbad5060312e065cd62d926dfa7a61689217b31
Caio Sanches <ccampato@gmail.com>
2018-04-29 22:25:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,7 @@ class Song
     @@genres
   end
 
-  def self.artist
+  def self.artists
     artist_array = []
     @@artists.each do |artist|
       artist_array << artist unless artist_array.include?(artist)
